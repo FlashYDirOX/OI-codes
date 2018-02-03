ef1d0bd26b3dca8a95d87241a7a60959cc4484e3
flashydirox <FlashYDirOX@gmail.com>
2018-02-03 21:49:28 +0000

    No commit

---
+++ exercises/P1010幂次方.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+#endif
+}
--- exercises/P1141-01迷宫.cpp
+++ exercises/P1141-01迷宫.cpp
@@ -3,10 +3,10 @@
 const int MAXN = 1010;
 const int d[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-int N, M;
+int N, M, cnt;
 int G[MAXN][MAXN], value[MAXN][MAXN], cx[MAXN * MAXN], cy[MAXN * MAXN];
 
-inline void bfs(int x, int y);
+inline void dfs(const int& x, const int& y);
 
 int main()
 {
@@ -23,35 +23,34 @@ int main()
 
 	for (register int i = 1; i <= M; i++) {
 		int p, q;
+		cnt = 0;
 		
 		scanf("%d%d", &p, &q);
-		if (!value[p][q])
-			bfs(p, q);
+
+		if (!value[p][q]) {
+			dfs(p, q);
+			for (register int i = 0; i <= cnt; i++)
+				value[cx[i]][cy[i]] = cnt;
+		}
+
 		printf("%d\n", value[p][q]);
 	}
 
 	return 0;
 }
 
-inline void bfs(int x, int y)
+inline void dfs(const int&  x, const int& y)
 {
-	int head = 0, tail = 1;
-
-	cx[head] = x, cy[head] = y;
+	cnt++;
+	cx[cnt] = x, cy[cnt] = y;
 	value[x][y] = 1;
 
-	while (head < tail) {
-		for (register int i = 0; i < 4; i++) {
-			int u = cx[head] + d[i][0], v = cy[head] + d[i][1];
-			if (u < 1 || v < 1 || u > N || v < N || value[u][v] || G[u][v] == G[cx[head]][cy[head]])
-				continue ;
-			tail++;
-			cx[tail] = u;
-			cy[tail] = v;
-		}
-		head++;
-	}
+	for (register int i = 0; i < 4; i++) {
+		int u = x + d[i][0], v = y + d[i][1];
+		if (u < 1 || v < 1 || u > N || v > N || value[u][v] || G[u][v] == G[x][y])
+			continue ;
+		value[u][v] = 1;
 
-	for (register int i = 0; i <= tail; i++)
-		value[cx[i]][cy[i]] = tail;
+		dfs(u, v);
+	}
 }
