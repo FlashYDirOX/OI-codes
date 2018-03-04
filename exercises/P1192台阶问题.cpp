4a19ee36440f78e517fc53b7537d828728136452
flashydirox <FlashYDirOX@gmail.com>
2018-03-04 19:31:47 +0000

    No commit

---
+++ exercises/P1192台阶问题.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+#endif
+
+	int N, K;
+
+	scanf("%d%d", &N, &K);
+
+	
+	
+	return 0;
+}
