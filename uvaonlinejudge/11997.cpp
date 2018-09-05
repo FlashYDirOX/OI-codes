46bb321dc0715eb6603200c23b6486089601a61a
flashydirox <FlashYDirOX@gmail.com>
2018-09-05 23:29:47 +0000

    No commit

---
+++ uvaonlinejudge/11997.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <queue>
+
+using std::queue;
+
+const int MAXN = 751;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+#endif
+
+	int k;
+
+	while (scanf("%d", &k)) {
+		
+	}
+
+
+	return 0;
+}
