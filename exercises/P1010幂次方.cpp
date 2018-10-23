cb16cef7e48cccd4164d3e66e4b9dbeecc634f55
flashydirox <flashydirox@gmail.com>
2018-10-23 23:20:22 +0800

    regular

--- exercises/P1010幂次方.cpp
+++ exercises/P1010幂次方.cpp
@@ -1,4 +1,13 @@
 #include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+inline string calc ( int x, int i );
 
 int main()
 {
@@ -6,4 +15,22 @@ int main()
 	freopen("in", "r", stdin);
 	freopen("out", "w", stdout);
 #endif
+
+	scanf ("%d", &N);
+
+	cout << calc(N, 0);
+
+	return 0;
+}
+
+inline string calc ( int x, int i )
+{
+	string s;
+	if (x == 0)
+		return "0";
+	do 
+		if (x & 1)
+			s = (i == 1 ? "2" : "2(" + calc(i, 0) + ")") + (s == "" ? "" : "+") + s;
+	while (++i, x >>= 1);
+	return s;
 }
