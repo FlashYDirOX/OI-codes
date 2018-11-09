6ab4c54eb68e7dca875744403fa342934b133961
flashydirox <flashydirox@gmail.com>
2018-11-09 11:51:07 +0800

    regular updated

--- exercises/P1196银河英雄传说.cpp
+++ exercises/P1196银河英雄传说.cpp
@@ -1,12 +1,18 @@
 #include <cstdio>
 #include <cctype>
+#include <cmath>
+#include <iostream>
 
 #define REP(i, a, b) for(register int i = a; i <= b; i++)
+
+using std::cin;
 
 const int MAXN = 30001;
 
 int T;
 int father[MAXN], head[MAXN], value[MAXN];
+
+inline int find ( int x );
 
 int main()
 {
@@ -19,15 +25,39 @@ int main()
 
 	scanf("%d", &T);
 
-	c = getchar();
+	REP(i, 1, 30001){
+		father[i] = i;
+		head[i] = 0;
+		value[i] = 1;
+	}
 
 	while(T--){
-		while(!isdigit(c) && c != 'M' && c != 'C')
-			c = getchar();
-		if(c == 'M')
+		int x, y, fx, fy;
 
+		cin >> c;
+
+		scanf("%d%d", &x, &y);
+		fx = find(x);
+		fy = find(y);
+		if(c == 'M'){
+			head[fx] += value[fy];
+			value[fy] += value[fx];
+			value[fx] = 0;
+			father[fx] = fy;
+		}
 		if(c == 'C')
+			fx != fy ? puts("-1") : printf("%d\n", abs(head[x] - head[y]) - 1);
 	}
 
 	return 0;
+}
+
+inline int find ( int x )
+{
+	int tmp;
+	if(father[x] == x)
+		return father[x];
+	tmp = find(father[x]);
+	head[x] += head[father[x]];
+	return father[x] = tmp;
 }
