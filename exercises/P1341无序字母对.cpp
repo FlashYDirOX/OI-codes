6f307586706388dafedd1c9e7607e7e281592373
flashydirox <flashydirox@gmail.com>
2019-08-04 22:00:14 +0800

    fuck

--- exercises/P1341无序字母对.cpp
+++ exercises/P1341无序字母对.cpp
@@ -1,43 +1,42 @@
-#include <cstdio>
-#include <algorithm>
-#include <cstdlib>
-#include <cstring>
-
-const int MAXN = 53;
-const int INF = 0x3f3f3f3f;
-
-int gree[MAXN], G[MAXN][MAXN];
-char s[MAXN];
-
-int main()
-{
-#ifndef ONLINE_JUDGE
-	freopen("in", "r", stdin);
-	freopen("out", "w", stdout);
-#endif
-
-	scanf("%d", &N);
-
-	rep(i, 1, N){
-		int a, b;
-		scanf("%s", s);
-		a = s[0] - 'A';
-		b = s[1] - 'A';
-		first = std::min(first, std::min(a, b));
-		gree[a]++;
-		gree[b]++;
-	}
-
-	rep(i, 0, MAXN)
-		if(gree[i] & 1
-			num1 ? num1 = i : num2 = i;
-
-	if(num1 == 0){
-		puts("No Solution");
-		exit(0);
-	}
-
-	if
-
-	return 0;
+#include<cstdio>
+#include<cstring>
+int a[106],c[10006],du[101],n,x,y,
+    k=0xfffffff,tot;
+bool b[106][106];
+char s[2];
+int min(int u,int v) {
+    return u<v ? u:v;
+}
+void dfs(int u) {
+    for(int i=0; i<58; i++) {
+        if(b[u][i]) {
+            b[u][i]=b[i][u]=0;
+            dfs(i);
+        }
+    }
+    c[++tot]=u;
+}
+int main() {
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++) {
+        scanf("%s",&s);
+        x=s[0]-'A';
+        y=s[1]-'A';
+        k=min(k,min(x,y));
+        b[x][y]=b[y][x]=1;
+        du[x]++;
+        du[y]++;
+    }
+    for(int i=0; i<58; i++)
+        if(du[i]%2==1)
+            a[++a[0]]=i;
+    if(a[0]==0) dfs(k);
+    else if(a[0]==2) dfs(a[1]);
+    else    {
+        printf("No Solution\n");
+        return 0;
+    }
+    for(int i=tot; i>=1; i--)
+        printf("%c",c[i]+'A');
+    return 0;
 }
---
+++ exercises/P2921USACO08DEC.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+#define rep(i, a, b) for(register int i = a; i <= b; i++)
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+#endif
+
+	scanf("%d", &N);
+
+	rep(i, 1, N)
+
+	return 0;
+}
