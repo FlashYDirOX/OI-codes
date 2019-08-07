751e383f77fab75e376235b4c8a00b415e816a7e
flashydirox <flashydirox@gmail.com>
2019-08-07 22:01:09 +0800

    Asshole

---
+++ exercises/P1087FBI树.cpp
@@ -0,0 +1,67 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+#define rep(i, a, b) for(register int i = a; i <= b; i++)
+
+const int MAXN = 11;
+
+int N, cnt;
+char s[2 << MAXN], ans[2 << MAXN];
+
+inline void dfs ( int, int );
+
+inline char pd ( int, int );
+
+int main()
+{
+#ifndef ONLINEflagJUDGE
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+#endif
+
+	int len, bit = 0;
+
+	scanf("%d", &N);
+
+	scanf("%s", s);
+	
+	len = strlen(s);
+
+	while(len != 1){
+		len >>= 1;
+		bit++;
+	}
+
+	dfs(0, bit);
+
+	rep(i, 1, ( 1 << (bit + 1)) - 1) putchar(ans[i]); 
+
+	return 0;
+}
+inline void dfs ( int start, int step )
+{
+	if(step < 0)
+		return ;
+	dfs(start, step - 1);
+	dfs(start + (1 << (step - 1)), step - 1);
+	ans[++cnt] = pd(start, step);
+}
+
+inline char pd ( int start, int step )
+{
+	int num = start + ( 1 << step );
+	bool flag0 = 0, flag1 = 0;
+	rep(i, start, num - 1){
+		if(s[i] == '0')
+			flag0 = true;
+		if(s[i] == '1')
+			flag1 = true;
+	}
+	if(flag0 && flag1)
+		return 'F';
+	if(flag0)
+		return 'B';
+	if(flag1)
+		return 'I';
+}
--- exercises/P2921USACO08DEC.cpp
+++ exercises/P2921USACO08DEC.cpp
@@ -2,6 +2,13 @@
 
 #define rep(i, a, b) for(register int i = a; i <= b; i++)
 
+const int MAXN = 100001;
+
+int G[MAXN], f[MAXN], deep[MAXN], N;
+bool flag[MAXN];
+
+inline void dfs ( int, int );
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -12,6 +19,35 @@ int main()
 	scanf("%d", &N);
 
 	rep(i, 1, N)
+		scanf("%d", &G[i]);
+
+	rep(i, 1, N)
+		if(!flag[i])
+			dfs(i, 0);
+
+	rep(i, 1, N)
+		printf("%d\n", f[i]);
 
 	return 0;
+}
+
+inline void dfs ( int i, int step )
+{
+	flag[i] = true;
+	deep[i] = step;
+	if(f[G[i]])
+		f[i] = f[G[i]] + 1;
+	else if(deep[G[i]]){
+		f[i] = ( deep[i] - deep[G[i]] + 1 );
+		int num = G[i];
+		while(num != i){
+			f[num] = f[i];
+			num = G[num];
+		}
+	}
+	else{
+		dfs(G[i], step + 1);
+		if(!f[i])
+			f[i] = f[G[i]] + 1;
+	}
 }
