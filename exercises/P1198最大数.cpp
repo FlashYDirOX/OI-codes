589f42518f7002fb67879e8cf0b2a9a5adee71dd
flashydirox <flashydirox@gmail.com>
2019-11-10 10:31:26 +0800

    update

--- exercises/P1198最大数.cpp
+++ exercises/P1198最大数.cpp
@@ -1,15 +1,19 @@
 #include <cstdio>
+#include <algorithm>
 
 #define rep(i, a, b) for(register int i = a; i <= b; i++)
-#define lson ( rt << 1, head, mid)
-#define rson ( rt << 1 | 1, mid + 1, tail)
+#define lson rt << 1, head, mid
+#define rson rt << 1 | 1, mid + 1, tail
 #define mid ( ( head + tail ) >> 1 )
 
 typedef long long ll;
 
 const int MAXN = 200001;
+const int INF = 0x3f3f3f3f;
 
-ll N, MOD, a[MAXN], cnt, sum[MAXN];
+using std::max;
+
+ll M, MOD, a[MAXN], cnt, sum[MAXN];
 char s[10];
 
 inline ll query ( int, int, int, int, int );
@@ -23,15 +27,15 @@ int main()
 	freopen("out", "w", stdout);
 #endif
 
-	scanf("%d%d", &M, &MOD);
+	scanf("%lld%lld", &M, &MOD);
 
 	rep(i, 1, M){
 		int L;
 		scanf("%s%d", s, &L);
 		if(s[0] == 'Q')
-			printf("%d\n", cnt = query(1, 1, N, N - L + 1, N));
+			printf("%lld\n", cnt = query(1, 1, M, M - L + 1, M));
 		if(s[0] == 'A')
-			insert(1, 1, N, L);
+			insert(1, 1, M, L);
 	}
 
 	return 0;
@@ -39,20 +43,27 @@ int main()
 
 inline ll query ( int rt, int head, int tail, int L, int R )
 {
-	int count;
-	if(head >= tail){
-		count += sum[rt];
-	}
+	int u = INF, v = INF;
+	if(head >= L && tail <= R)
+		return sum[rt];
+	if(L <= mid)
+		u = query(lson, L, R);
+	if(R > mid)
+		v = query(rson, L, R);
+	return std::max(u, v);
 }
 
 inline void insert ( int rt, int head, int tail, int node )
 {
 	if(head == tail && tail == node){
-		scanf("%d", &a[++cnt]);
+		scanf("%lld", &sum[rt]);
+		return ;
 	}
 	else {
-		insert(lson, node);
-		insert(rson, node);
+		if(node <= mid)
+			insert(lson, node);
+		if(node > mid)
+			insert(rson, node);
 		sum[rt] = sum[rt << 1] + sum[rt << 1 | 1];
 	}
 }
